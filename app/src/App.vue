18cc5a9d2ae1c650db19122866ddae6d9257153f
Alex Mellor <rollemxela@gmail.com>
2024-12-16 18:15:19 +0000

    run addScratchCovers() method on screen orientation change.

--- app/src/App.vue
+++ app/src/App.vue
@@ -19,6 +19,7 @@ export default {
   },
   mounted() {
     this.$nextTick(() => {
+      screen.orientation.addEventListener('change', this.addScratchCovers);
       window.addEventListener('load', this.addScratchCovers);
     });
   },
